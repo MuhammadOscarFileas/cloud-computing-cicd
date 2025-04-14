ab4db8a0c7806b54f90516016366c5b438729351
MorvyExpected <153181833+MuhammadOscarFileas@users.noreply.github.com>
2025-04-14 12:10:49 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("RECOVER_YOUR_DATA", "root", "",{
+const db = new Sequelize("api_catatan", "root", "",{
     host: "35.193.25.9",
     dialect: "mysql",
     logging: console.log,
